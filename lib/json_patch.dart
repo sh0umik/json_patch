8c97efd07c38ebc110838ba08002f591bc1f8231
Fahim Shariar <fahim.shoumik@gmail.com>
2019-01-28 19:50:38 +0600

    json patch implementation

--- lib/json_patch.dart
+++ lib/json_patch.dart
@@ -1,7 +1,199 @@
 library json_patch;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class JsonPatchOperation {
+  String op;
+  String path;
+  dynamic value;
+  JsonPatchOperation({this.op, this.path, this.value});
+
+  Map<String, dynamic> toJSON() {
+    if (value != null) {
+      return {"op": op, "path": path, "value": value};
+    } else {
+      return {
+        "op": op,
+        "path": path,
+      };
+    }
+  }
 }
+
+class JsonPatch {
+
+  List<JsonPatchOperation> buildPatches(Map oldJson, Map newJson){
+    return diff(oldJson, newJson, "");
+  }
+
+}
+
+List<JsonPatchOperation> diff(Map<String, dynamic> a, Map<String, dynamic> b, String path){
+
+  List<JsonPatchOperation> patches = <JsonPatchOperation>[];
+
+  b.forEach((key, bv){
+    var p = makePath(path, key);
+    var av = a[key];
+    if (av == null) {
+      patches.add(NewPatch("add", p, bv));
+    }
+
+    //if types have changed then replace completely
+    if(av.runtimeType != bv.runtimeType && av != null){
+      patches.add(NewPatch("replace", p, bv));
+    }
+
+    patches.addAll(handleValues(av, bv, p));
+  });
+
+  a.forEach((key, val){
+    var found = b[key];
+    if (found == null){
+      var p = makePath(path, key);
+      patches.add(NewPatch("remove", p, null));
+    }
+  });
+
+  return patches;
+}
+
+List<JsonPatchOperation> handleValues(dynamic av, dynamic bv, String p) {
+
+  List<JsonPatchOperation> patches = <JsonPatchOperation>[];
+
+  // Handle value if same type
+  if(av.runtimeType == bv.runtimeType) {
+    if (av is Map) {
+      patches.addAll(diff(av, bv, p));
+    } else if (av is String || av is double || av is int || av is bool) {
+      if (!matchValue(av, bv)) {
+        patches.add(JsonPatchOperation(op: "replace", path: p, value: bv));
+      }
+    } else if (av is List) {
+      var bt = bv;
+      var at = av;
+      if (bt == null) {
+        // array replaced by non-array
+        patches.add(NewPatch("replace", p, bv));
+      } else if (at.length != bt.length) {
+        // arrays are not the same length
+        patches.addAll(compareArray(at, bt, p));
+      } else {
+        for (var i = 0; i < bt.length; i++) {
+          patches = handleValues(at[i], bt[i], makePath(p, i));
+        }
+      }
+    } else if (av == null) {
+      if (av == null) {
+        // Both Fine
+      } else {
+        patches.add(NewPatch("add", p, bv));
+      }
+    } else {
+      print("Unknown type ");
+      print(av.runtimeType);
+    }
+  }
+  return patches;
+}
+
+List<JsonPatchOperation> compareArray(List<dynamic> av, List<dynamic> bv, String p){
+  List<JsonPatchOperation> retval = <JsonPatchOperation>[];
+
+  for (var i=0; i< av.length; i++){
+    bool found = false;
+    for(var j=0; j< bv.length; j++) {
+      if (av[i] == bv[j]) {
+        found = true;
+        break;
+      }
+    }
+    if(!found){
+      retval.add(NewPatch("remove",  makePath(p, i), null));
+    }
+  }
+
+  for (var i=0; i< bv.length; i++){
+    bool found = false;
+    for(var j=0; j< av.length; j++) {
+      if (av[j] == bv[i]) {
+        found = true;
+        break;
+      }
+    }
+    if(!found){
+      retval.add(NewPatch("add",  makePath(p, i), bv[i]));
+    }
+  }
+
+  return retval;
+}
+
+bool matchValue(dynamic av, dynamic bv) {
+  if (av.runtimeType != bv.runtimeType){
+    return false;
+  }
+  switch (av.runtimeType){
+    case String:
+      var bt = bv.toString();
+      if (bt == av) {
+        return true;
+      }
+      break;
+    case double:
+      var bt = bv.cast<double>();
+      if (bt == av) {
+        return true;
+      }
+      break;
+    case int:
+      var bt = bv;
+      if (bt == av) {
+        return true;
+      }
+      break;
+    case bool:
+      var bt = bv.cast<bool>();
+      if (bt == av) {
+        return true;
+      }
+      break;
+    case Map:
+      var bt = bv.cast<Map<String, dynamic>>();
+      var at = av.cast<Map<String, dynamic>>();
+      at.forEach((key, _){
+        if (!matchValue(key, bt[key])){
+          return false;
+        }
+      });
+      bt.forEach((key, _){
+        if (!matchValue(key, bt[key])){
+          return false;
+        }
+      });
+      return true;
+      break;
+    case List:
+    // todo
+      return true;
+      break;
+  }
+
+  return false;
+}
+
+
+JsonPatchOperation NewPatch(String operation, String path, dynamic value){
+  return JsonPatchOperation(op: operation, path: path, value: value);
+}
+
+String makePath(String path, dynamic newPart){
+  var key = newPart.toString();
+  if (path == "") {
+    return "/" + key;
+  }
+  if (path.endsWith("/")){
+    return path + key;
+  }
+  return path + "/" + key;
+}
+
--- test/json_patch_test.dart
+++ test/json_patch_test.dart
@@ -3,11 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:json_patch/json_patch.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('simple json value chagnes test', () {
+    final calculator = JsonPatch();
+    expect(calculator.buildPatches({"foo":"baz"}, {"foo":"baz1"}), [JsonPatchOperation(op: "replace", path: "/foo", value: "baz1")]);
+    //expect(calculator.addOne(-7), -6);
+    //expect(calculator.addOne(0), 1);
+    //expect(() => calculator.addOne(null), throwsNoSuchMethodError);
   });
 }
